ebf6ff79e4c59f3f7b676ce5bfdef76dcc47dd10
Leslie Ann Lim <Lalim@Leslies-MacBook-Air.local>
2016-05-24 16:52:28 -0700

    add createBoard

--- script.js
+++ script.js
@@ -12,25 +12,44 @@ var gameController = function (element){
             var tr = $('<tr>');
             var newRow = [];
             for (var j = 0; j < width; j++){
+                var newCell = new cellGenerator(colors[colorIndex],{x:i,y:j});
                 colorIndex = 1 - colorIndex;
+                var cell = newCell.renderCell();
+
             }
         self.element.append(tr);
         colorIndex = 1 - colorIndex;
         }
-    }
+    };
 
     var cellGenerator = function(color, position){
         var cellSelf = this;
         cellSelf.color = color;
         cellSelf.position = position;
-        cellSelf.cellElement = $('<td>')
-        )
-
+        cellSelf.currentPiece = null;
+        cellSelf.cellElement = null;
+        cellSelf.renderCell = function() {
+            cellSelf.cellElement = ('<td>').addClass('cell').css('background-color', cellSelf.color);
+            return cellSelf.cellElement;
+        };
+        cellSelf.cellPosition = function(){
+            return cellSelf.position;
+        };
+        cellSelf.setCurrentPiece = function(piece){
+            cellSelf.currentPiece = piece;
+        };
+        cellSelf.getCurrentPiece = function(piece){
+            return cellSelf.currentPiece;
+        };
+        cellSelf.getColor = function(){
+            return cellSelf.color;
+        }
     }
 
 
 };
 var game;
 $(document).ready(function(){
-   game = new gameController('#game');
+    game = new gameController('#board');
+    game.createBoard(8,8);
 });
